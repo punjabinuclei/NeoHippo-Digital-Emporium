c36a6893713f3e9082fea6041232cfe89159766c
punjabinuclei <guppi0204@gmail.com>
2023-12-17 18:26:21 +0530

    Navbar Done

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,20 +1,48 @@
 "use client";
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
+import NavItem from "./NavItem"
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
-const [activeIndex, setActiveIndex] = useState<null | number>(null)
+    const [activeIndex, setActiveIndex] = useState<null | number>(null)
+
+    const navRef = useRef<HTMLDivElement>(null);
+    useOnClickOutside(navRef, () => setActiveIndex(null));
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            if (e.key === "Escape") {
+                setActiveIndex(null);
+            }
+        }
+
+        document.addEventListener("keydown", handler);
+        return () => document.removeEventListener("keydown", handler);
+    })
 
     return (
-        <div className="flex gap-4 h-full">
-{/* {PRODUCT_CATEGORIES.map((category, index) => {
-const handleOpen=()=>{
-    if(activeIndex=== index)
-}
+        <div className="flex gap-4 h-full" ref={navRef}>
+            {PRODUCT_CATEGORIES.map((category, index) => {
+                const handleOpen = () => {
+                    if (activeIndex === index) {
+                        setActiveIndex(null);
+                    }
+                    else {
+                        setActiveIndex(index);
+                    }
+                }
+                const isAnyOpen = activeIndex !== null;
+                const isOpen = index === activeIndex;
 
-
-    return();
-})} */}
+                return (<NavItem
+                    isAnyOpen={isAnyOpen}
+                    isOpen={isOpen}
+                    handleOpen={handleOpen}
+                    category={category}
+                    key={category.value}
+                />);
+            })}
         </div>
     )
 }
